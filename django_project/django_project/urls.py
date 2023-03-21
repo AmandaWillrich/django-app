3a52073415397c0dca26ea5724ea8ee88d8fcbc3
Amanda Willrich Fuchs <111703865+AmandaWillrich@users.noreply.github.com>
2023-03-21 11:26:09 -0300

    Added comments to some files

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -29,5 +29,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
 
+# Necessary to make static work
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_project/users/signals.py
+++ django_project/users/signals.py
@@ -11,3 +11,9 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+# Saving the user sets a signal to the receiver, 
+# which creates and saves a profile automatically using the methods above.
+# The Django documentation sugests a creation of the signals.py file
+# to set the signals, although some people put this in the 
+# models (not recommended).
